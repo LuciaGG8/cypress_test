ef2f610ff56368676e4082e81a0c652953790ffb
LuciaGG8 <you@example.com>
2022-11-07 18:38:43 +0100

    ejercicios

---
+++ cypress/integration/login.cy.js
@@ -0,0 +1,66 @@
+/// <reference types="cypress" />
+
+
+it ('A valid user can login', () =>{
+    cy.visit("https://the-internet.herokuapp.com/")
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type("tomsmith")
+    cy.get('#password').type("SuperSecretPassword!")
+    cy.get('.fa').click()
+    cy.get('#flash').contains("You logged into a secure area!")
+
+})
+
+it('A invalid password cannot login', () =>{
+
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type("tomsmith")
+    cy.get('#password').type("error!")
+    cy.get('.fa').click()
+    cy.get('#flash').contains("Your password is invalid!")
+  
+  
+  
+  
+     })
+
+   
+     it('A invalid usename cannot login', () =>{
+
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get(':nth-child(21) > a').click()
+        cy.get('#username').type("errot!")
+        cy.get('#password').type("SuperSecretPassword!")
+        cy.get('.fa').click()
+        cy.get('#flash').contains("Your username is invalid!")
+      
+      
+      
+      
+         })
+    
+     
+         it("invalid login", () =>{
+            cy.visit("https://the-internet.herokuapp.com")
+            cy.get(':nth-child(21) > a').click()
+            cy.get('#username')
+            cy.get('#password')
+            cy.get('.fa').click()
+            cy.get('#flash').contains("Your username is invalid!")
+        })
+
+
+        /// <reference types="cypress" />
+
+it('Shifting content', () =>{    
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39) > a').click()
+    cy.get('[href="/shifting_content/menu"]').click()
+    cy.get('li').should('have.length',5)
+})
+        
+        
+        
+
+
