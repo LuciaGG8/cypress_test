bf65929c4b814a212b281b393bdaf7638a561f92
LuciaGG8 <you@example.com>
2022-11-07 19:18:01 +0100

    describe y beforeEach

--- cypress/integration/login.cy.js
+++ cypress/integration/login.cy.js
@@ -1,54 +1,51 @@
 /// <reference types="cypress" />
 
-
-it ('A valid user can login', () =>{
+describe('TESTS DE LOGIN', () => {
+  beforeEach(() => {
     cy.visit("https://the-internet.herokuapp.com/")
     cy.get(':nth-child(21) > a').click()
+
+     })
+
+  it ('A valid user can login', () =>{
+    
     cy.get('#username').type("tomsmith")
     cy.get('#password').type("SuperSecretPassword!")
     cy.get('.fa').click()
     cy.get('#flash').contains("You logged into a secure area!")
 
-})
+     })
 
-it('A invalid password cannot login', () =>{
+  it('A invalid password cannot login', () =>{
 
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(':nth-child(21) > a').click()
     cy.get('#username').type("tomsmith")
     cy.get('#password').type("error!")
     cy.get('.fa').click()
     cy.get('#flash').contains("Your password is invalid!")
   
-  
-  
-  
-     })
+    })
 
    
-     it('A invalid usename cannot login', () =>{
+  it('A invalid usename cannot login', () =>{
 
-        cy.visit('https://the-internet.herokuapp.com/')
-        cy.get(':nth-child(21) > a').click()
-        cy.get('#username').type("errot!")
-        cy.get('#password').type("SuperSecretPassword!")
-        cy.get('.fa').click()
-        cy.get('#flash').contains("Your username is invalid!")
+    cy.get('#username').type("errot!")
+    cy.get('#password').type("SuperSecretPassword!")
+    cy.get('.fa').click()
+    cy.get('#flash').contains("Your username is invalid!")
       
-      
-      
-      
-         })
+    })
     
      
-         it("invalid login", () =>{
-            cy.visit("https://the-internet.herokuapp.com")
-            cy.get(':nth-child(21) > a').click()
-            cy.get('#username')
-            cy.get('#password')
-            cy.get('.fa').click()
-            cy.get('#flash').contains("Your username is invalid!")
-        })
+  it("invalid login", () =>{
+ 
+    cy.get('#username')
+    cy.get('#password')
+    cy.get('.fa').click()
+    cy.get('#flash').contains("Your username is invalid!")
+   
+    })
+
+ })
 
 
         /// <reference types="cypress" />
